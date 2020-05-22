c2e6ecf1675b7a0d1d89d9278422446692d9ebbf
Bjorn Stabell <bjorn@stabell.org>
2020-05-22 14:43:41 -0700

    These hacks are apparently no longer needed.  Now, instead, you'll get an unused warning.

--- tiptap-svelte-extensions/src/nodes/TodoItem.svelte
+++ tiptap-svelte-extensions/src/nodes/TodoItem.svelte
@@ -3,13 +3,6 @@
   export let updateAttrs;
   export let view;
 
-  // HACK: These are here to stop warnings clogging up the debug console
-  export let editor;
-  export let getPos;
-  export let decorations;
-  export let selected;
-  export let options;
-
   // TODO: Figure out a way to get this out of the component
   // For now, we require an element with the class `content-xx`
   export let content;
